a499121c6c35467ddaee198729150f17e8aa9a67
Chen Sicong <sicong.chen@163.com>
2020-08-11 18:08:11 +0800

    [Chen Sicong] feat: add educations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import MyTitle from './compnents/title/my-title';
 import About from './compnents/about/about';
+import Education from './compnents/education/education';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <MyTitle />
         <hr style={{ border: '3px solid', color: '#e3e3e3' }} />
         <About />
+        <Education />
       </main>
     );
   }
