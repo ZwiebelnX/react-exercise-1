94077917e8d175d6e6a0ef190c0568ca720ccf74
Chen Sicong <sicong.chen@163.com>
2020-08-11 17:27:40 +0800

    [Chen Sicong] feat: add about me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import MyTitle from './compnents/title/my-title';
+import About from './compnents/about/about';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <main className="app">
         <MyTitle />
         <hr style={{ border: '3px solid', color: '#e3e3e3' }} />
+        <About />
       </main>
     );
   }
