dca4a25619402887eed80340bf1e114407bbfe96
Chen Sicong <sicong.chen@163.com>
2020-08-11 17:19:54 +0800

    [Chen Sicong] feat: add title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
+import MyTitle from './compnents/title/my-title';
 
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return (
+      <main className="app">
+        <MyTitle />
+        <hr style={{ border: '3px solid', color: '#e3e3e3' }} />
+      </main>
+    );
   }
 }
 
